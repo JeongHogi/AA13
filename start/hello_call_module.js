a545c6dcd64d32b056706fffef7bea445ec93d9f
JeongHogi <127375422+JeongHogi@users.noreply.github.com>
2023-09-14 16:48:55 +0900

    Delete start directory

--- start/hello_call_module.js
+++
@@ -1,4 +0,0 @@
-var olleh = require('./hello_user_module');
-
-olleh("Node");
-olleh("AA13");
--- start/hello_function.js
+++
@@ -1,7 +0,0 @@
-// hello_function.js
-function hello(what) {
-    console.log("Hello " + what +" !");
-}
-
-hello("AA13");
-hello("Redwoods, 정호기");
